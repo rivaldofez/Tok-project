138e6a67a31c787b3635eead88f50470370e1a9b
bagasbest <bagasbest80@gmail.com>
2020-09-01 09:53:04 +0700

    base project

---
+++ SeTrip/app/src/main/java/com/tokproject/setrip/activity/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.tokproject.setrip.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tokproject.setrip.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+    }
+}
