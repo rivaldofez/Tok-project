ad1348821e33720e4089a7e2e2e9d95d806d9052
bagasbest <bagasbest80@gmail.com>
2020-09-03 14:32:22 +0700

    fix

--- SeTrip/app/src/main/java/com/tokproject/setrip/fragment/ProfileFragment.java
+++ SeTrip/app/src/main/java/com/tokproject/setrip/fragment/ProfileFragment.java
@@ -4,7 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+<<<<<<< Updated upstream
+=======
+import android.provider.MediaStore;
+import android.provider.Settings;
+>>>>>>> Stashed changes
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,8 +42,53 @@ public class ProfileFragment extends Fragment {
 
         btnLogout = view.findViewById(R.id.btnLogout);
         firebaseAuth = FirebaseAuth.getInstance();
+<<<<<<< Updated upstream
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
+=======
+        user = firebaseAuth.getCurrentUser();
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("User");
+        storageReference = FirebaseStorage.getInstance().getReference();
+
+        //init array of permission
+        cameraPermission = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        storagePermission = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        TextView viewTv = view.findViewById(R.id.viewTv);
+        selamatTv = view.findViewById(R.id.selamatTv);
+        ImageView changeProfilePicture = view.findViewById(R.id.changePictureIv);
+        backgroundIv = view.findViewById(R.id.backgroundIv);
+        nameTv = view.findViewById(R.id.usernameTv);
+        emailTv = view.findViewById(R.id.emailTv);
+        phoneNumber = view.findViewById(R.id.phonenbrTv);
+        noteTv = view.findViewById(R.id.noteTv);
+        progressBar = view.findViewById(R.id.progressBar);
+        detail = view.findViewById(R.id.selamatBekerjaTv);
+        avatarIv = view.findViewById(R.id.avatarIv);
+
+        checkUserStatus();
+
+        loadDataUser();
+
+
+
+        viewTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewAllTourismLocation();
+            }
+        });
+
+        changeProfilePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                changeProfilePic();
+            }
+        });
+
+        noteTv.setOnClickListener(new View.OnClickListener() {
+>>>>>>> Stashed changes
             @Override
             public void onClick(View v) {
                 firebaseAuth.signOut();
@@ -48,6 +99,248 @@ public class ProfileFragment extends Fragment {
         return view;
     }
 
+<<<<<<< Updated upstream
+=======
+    private void showNoteTv() {
+        startActivity(new Intent(getActivity(), NoteReminderActivity.class));
+    }
+
+    private void loadDataUser() {
+        showLoader(true);
+
+        Query query = databaseReference.orderByChild("email").equalTo(user.getEmail());
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot ds: snapshot.getChildren()) {
+                    //get data
+                    String name = ""+ds.child("username").getValue();
+                    String email = ""+ds.child("email").getValue();
+                    String phone_nbr = ""+ds.child("phone_nbr").getValue();
+                    String image = ""+ds.child("image").getValue();
+
+                    //setData
+                    nameTv.setText(name);
+                    emailTv.setText(email);
+                    phoneNumber.setText(phone_nbr);
+
+                    Glide.with(ProfileFragment.this).load(image)
+                            .error(R.drawable.avatar_blank)
+                            .placeholder(R.drawable.avatar_blank)
+                            .into(avatarIv);
+
+                    showLoader(false);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void changeProfilePic() {
+        profile = "image";
+        String[] options = {"Kamera", "Galeri"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.pilih_mode);
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                if(which == 0) {
+                    //camera clicked
+                    if(!checkCameraPermission()){
+                        requestCameraPermission();
+                    } else  {
+                        pickFromCamera();
+                    }
+                } else if (which == 1) {
+                    //galeri clicked
+                    if(!checkStoragePermission()) {
+                        requestStoragePermission();
+                    } else {
+                        pickFromGallery();
+                    }
+                }
+
+            }
+        });
+        builder.create().show();
+    }
+
+    private boolean checkCameraPermission() {
+        boolean result = ContextCompat.checkSelfPermission(Objects.requireNonNull(getActivity()), Manifest.permission.CAMERA)
+                == (PackageManager.PERMISSION_GRANTED);
+        boolean result1 = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == (PackageManager.PERMISSION_GRANTED);
+        return  result && result1;
+    }
+
+    private boolean checkStoragePermission() {
+        return ContextCompat.checkSelfPermission(Objects.requireNonNull(getActivity()), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == (PackageManager.PERMISSION_GRANTED);
+    }
+
+    private void requestCameraPermission() {
+        requestPermissions(cameraPermission, CAMERA_REQUEST_CODE);
+    }
+
+    private void requestStoragePermission() {
+        requestPermissions(storagePermission, STORAGE_REQUEST_CODE);
+    }
+
+    private void pickFromCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "Temp Pict");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Temp Description");
+
+        //put image uri
+        image_uri = Objects.requireNonNull(getActivity()).getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                values);
+
+        //intent to start camera
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        startActivityForResult(cameraIntent, IMAGE_PICK_CAMERA_CODE);
+    }
+
+    private void pickFromGallery() {
+        Intent galeryIntent = new Intent(Intent.ACTION_PICK);
+        galeryIntent.setType("image/*");
+        startActivityForResult(galeryIntent, IMAGE_PICK_GALLERY_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case  CAMERA_REQUEST_CODE: {
+                if(grantResults.length > 0) {
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean writeStorageAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                    if(cameraAccepted && writeStorageAccepted) {
+                        pickFromCamera();
+                    } else {
+                        Toast.makeText(getActivity(), R.string.mohon_setujui_kamera, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            break;
+
+            case STORAGE_REQUEST_CODE: {
+                if(grantResults.length > 0) {
+                    boolean writeStorageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+
+                    if(writeStorageAccepted) {
+                        pickFromGallery();
+                    } else {
+                        Toast.makeText(getActivity(), R.string.mohon_setujui_galeri, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+            break;
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(resultCode == RESULT_OK) {
+
+            if(requestCode == IMAGE_PICK_GALLERY_CODE) {
+                //image is picked from gallery, get uri image
+                assert data != null;
+                image_uri = data.getData();
+                uploadProfileCoverPhoto(image_uri);
+            }
+
+            if(requestCode == IMAGE_PICK_CAMERA_CODE) {
+                uploadProfileCoverPhoto(image_uri);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void uploadProfileCoverPhoto(Uri uri) {
+        // show proggress bar
+        showLoader(true);
+
+        //path and name of image ti be stored in firebase storage
+        String filePathAndName = storagePath + ""+ profile + "_" + user.getUid();
+
+        StorageReference storageReference2nd = storageReference.child(filePathAndName);
+        storageReference2nd.putFile(uri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+
+                        while (!uriTask.isSuccessful());
+                        final Uri downloadUri = uriTask.getResult();
+
+                        //check if image is uploaded or not and uri is received
+                        if(uriTask.isSuccessful()) {
+                            //image uploaded
+                            //add / update uri in user's database
+                            HashMap<String, Object> result = new HashMap<>();
+                            result.put(profile, downloadUri.toString());
+
+                            databaseReference.child(user.getUid()).updateChildren(result)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            showLoader(false);
+                                            Toast.makeText(getActivity(), R.string.gambar_diperbarui, Toast.LENGTH_SHORT).show();
+                                        }
+                                    }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    showLoader(false);
+                                    Toast.makeText(getActivity(), R.string.gambar_tidak, Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
+                        } else {
+                            showLoader(false);
+                            Toast.makeText(getActivity(), R.string.gangguan, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                showLoader(false);
+                Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void greetings() {
+        Calendar calendar = Calendar.getInstance();
+        int timeOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+
+        if(timeOfDay > 4 && timeOfDay < 12) {
+            backgroundIv.setBackgroundResource(R.drawable.header_morning);
+            selamatTv.setText(R.string.selamat_pagi);
+        } else if(timeOfDay >= 12 && timeOfDay < 18)  {
+            backgroundIv.setBackgroundResource(R.drawable.header_morning);
+            selamatTv.setText(R.string.selamat_siang);
+        } else if (timeOfDay > 18 || timeOfDay <4){
+            backgroundIv.setBackgroundResource(R.drawable.header_night);
+            selamatTv.setTextColor(Color.parseColor("#FFFFFF"));
+            detail.setTextColor(Color.parseColor("#FFFFFF"));
+            selamatTv.setText(R.string.selamat_malam);
+        }
+    }
+
+
+    private void viewAllTourismLocation() {
+        startActivity(new Intent(getActivity(), MapsLocationActivity.class));
+    }
+
+>>>>>>> Stashed changes
     private void checkUserStatus() {
         //get current user
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
@@ -56,7 +349,42 @@ public class ProfileFragment extends Fragment {
             uid = user.getUid();
         } else {
             startActivity(new Intent(getActivity(), Login.class));
+<<<<<<< Updated upstream
             getActivity().finish();
+=======
+            Objects.requireNonNull(getActivity()).finish();
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.option_menu, menu);
+        //hide add group icon
+        menu.findItem(R.id.option_language).setVisible(false);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.option_setting) {
+            startActivity(new Intent(getActivity(), SettingProfileActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showLoader(boolean b) {
+        if(b) {
+            progressBar.setVisibility(View.VISIBLE);
+        } else {
+            progressBar.setVisibility(View.GONE);
+>>>>>>> Stashed changes
         }
     }
 }
