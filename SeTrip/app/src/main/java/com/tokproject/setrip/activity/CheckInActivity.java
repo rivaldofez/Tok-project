190f18539534c2ba45687781678423b3874df1de
rivaldofez <rivaldofez@gmail.com>
2020-09-03 15:23:18 +0700

    make view QR check in QR

---
+++ SeTrip/app/src/main/java/com/tokproject/setrip/activity/CheckInActivity.java
@@ -0,0 +1,14 @@
+package com.tokproject.setrip.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_in);
+    }
+}
